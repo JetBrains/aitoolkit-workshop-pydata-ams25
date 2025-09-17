369a5710ae0833423233556a6ed61e8e92662848
Yaroslav Sokolov <yaroslav.sokolov@jetbrains.com>
2025-09-17 16:59:33 +0200

    Updated Code model a bit

--- agent/data_model.py
+++ agent/data_model.py
@@ -4,7 +4,7 @@ from langchain_core.pydantic_v1 import BaseModel, Field
 class Code(BaseModel):
     """Code output"""
 
+    description = "Schema for code solutions to coding questions split into 3 parts: Problem description, Imports and the Code block itself"
     prefix: str = Field(description="Description of the problem and approach")
     imports: str = Field(description="Code block import statements")
     code: str = Field(description="Code block not including import statements")
-    description = "Schema for code solutions to questions about LCEL."
